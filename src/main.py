d5708c170d54371941fe8b372229ee2ae58f2fc4
Shaun Oates <shaunoates@Shauns-MacBook.local>
2018-04-29 14:01:58 +0100

    try a simple template

--- src/main.py
+++ src/main.py
@@ -37,7 +37,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     form = AddressQuery()
-    return render_template('/usr/src/app/templates/address_query.html', title='BitCoin Address', form=form)
+    #return render_template('/usr/src/app/templates/address_query.html', title='BitCoin Address', form=form)
+    return render_template('/usr/src/app/templates/simple.html', title='BitCoin Address')
 
 
 if __name__ == '__main__':
