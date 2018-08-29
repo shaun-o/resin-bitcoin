223dc64b2945d19a4ea4d3f5441c451dcc7a736b
Shaun Oates <shaunoates@Shauns-MacBook.local>
2018-08-29 16:11:19 +0100

    Working on docker on mac

--- src/flask_app.py
+++ src/flask_app.py
@@ -4,10 +4,11 @@ import os
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+host=os.environ['redis']
 
 app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
+    CELERY_BROKER_URL='redis://{0}:6379'.format(host),
+    CELERY_RESULT_BACKEND='redis://{0}:6379'.format(host)
 )
 
 
